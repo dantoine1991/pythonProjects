4a57f0f9ff62cd611e7c2f45bbb3716aa373835a
David <davidantoine1991@gmail.com>
2022-03-13 19:17:05 -0400

    This is a script to follow the slack python project 1

---
+++ slackPythonProject1.py
@@ -0,0 +1,6 @@
+#This is made to build a project from 20220306 
+
+# PROJECT: Create a python script that says hi to everyone in this channel, no human interaction required, 
+# on different days of the week. The message should state 
+# "Hey <name>, today is <day, month, year>, have a great day!"
+
